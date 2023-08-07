650ab9be7b306ce4c6146ea9ad771a77addc7486
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-08-07 23:00:50 +0800

    xdiff 基本思路和数据结构

---
+++ examples/config.rs
@@ -0,0 +1,10 @@
+use anyhow::Result;
+use rust_xdiff::DiffConfig;
+
+fn main() -> Result<()> {
+    let content = include_str!("../fixtures/test.yml");
+    let config = DiffConfig::from_yaml(content)?;
+
+    println!("config: {:#?}", config);
+    Ok(())
+}
